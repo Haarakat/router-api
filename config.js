5124acb9ccfece9e894958703be364d861a40008
haarakat <haarakat.net@gmail.com>
2024-12-16 02:31:10 +0200

    Initial commit for Router API

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+    host: '192.168.88.1',      // عنوان IP الخاص بالميكروتك
+    username: 'api',         // اسم المستخدم
+    password: '1988',      // كلمة المرور
+    port: 8728                 // المنفذ الافتراضي للـ API
+};
+
+module.exports = config;
